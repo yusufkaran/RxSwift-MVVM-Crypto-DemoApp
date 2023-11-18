b19d3a61eb4b3158af0301c9305ee715d3df6d9f
Yusuf Karan <karan.yusuf.20@gmail.com>
2023-11-18 15:34:14 +0300

    added dummy json and complated webservices

--- CryptoRxMVVM/View/ViewController.swift
+++ CryptoRxMVVM/View/ViewController.swift
@@ -11,23 +11,37 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
   @IBOutlet weak var tableView: UITableView!
   
+  var cryptoList = [Crypto]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     tableView.dataSource = self
     tableView.delegate = self
+    
+    let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!
+    Webservice().downloadCurrencies(url: url) { result in
+      switch result {
+      case .success(let cryptos):
+        self.cryptoList = cryptos
+        DispatchQueue.main.async {
+          self.tableView.reloadData()
+        }
+      case .failure(let error):
+        print(error)
+      }
+    }
   }
   
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return cryptoList.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = UITableViewCell()
     var content = cell.defaultContentConfiguration()
-    content.text = "Crypto Currency"
-    content.secondaryText = "Crypto Price"
+    content.text = cryptoList[indexPath.row].currency
+    content.secondaryText = cryptoList[indexPath.row].price
     cell.contentConfiguration = content
     return cell
   }
 }
-
